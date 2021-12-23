ec98e254a172939844783a1fcabeea57cdba6b8e
Eduardo Oliveira Rodrigues <edu.ol.ro@gmail.com>
2021-12-22 23:42:07 -0300

    vídeo novo
    https://youtu.be/RVnV6lJYdKU

---
+++ python_video_aulas/integracao_noticias/minhas_noticias.php
@@ -0,0 +1,3 @@
+
+Temos a notícia de titulo "Adeus Corona"</br>Onde o texto é: <strong>Coronavirus é extinto</strong></br>Postada por: <strong>Eduardo</strong>
+Temos a notícia de titulo "Corona volta com tudo"</br>Onde o texto é: <strong>Coronavirus não é extinto</strong></br>Postada por: <strong>Eduardo</strong>
